77d0333d8006516ebe06dd546995977d1d569975
Nick Nestle <nick.nestle@gmail.com>
2025-02-28 09:37:47 -0800

    Integrated builder sdk

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
-let userConfig = undefined
+import BuilderDevTools from "@builder.io/dev-tools/next";
+
+let userConfig = undefined;
 try {
-  userConfig = await import('./v0-user-next.config')
+  userConfig = await import("./v0-user-next.config");
 } catch (e) {
   // ignore error
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = BuilderDevTools()({
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -21,28 +23,25 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-}
-
-mergeConfig(nextConfig, userConfig)
-
+});
+mergeConfig(nextConfig, userConfig);
 function mergeConfig(nextConfig, userConfig) {
   if (!userConfig) {
-    return
+    return;
   }
-
   for (const key in userConfig) {
     if (
-      typeof nextConfig[key] === 'object' &&
+      typeof nextConfig[key] === "object" &&
       !Array.isArray(nextConfig[key])
     ) {
       nextConfig[key] = {
         ...nextConfig[key],
         ...userConfig[key],
-      }
+      };
     } else {
-      nextConfig[key] = userConfig[key]
+      nextConfig[key] = userConfig[key];
     }
   }
 }
 
-export default nextConfig
+export default nextConfig;
